fe58e1c0cc048b206f7a3f0ffbd8401f6cb827d8
Antoine64160 <maltntoine64160@gmail.com>
2022-11-14 14:11:00 +0100

    init

---
+++ Express-Quests/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+const port = 5010;
+
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite movie list");
+};
+
+app.get("/", welcome);
+
+const movieHandlers = require("./movieHandlers");
+const users = require("./users");
+
+app.get("/api/movies", movieHandlers.getMovies);
+
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+app.get("/api/users", users.getUsers);
+
+app.get("/api/users/:id", users.getUserById);
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
